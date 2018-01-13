d6a0cc4b9cb1ba1d3e21c027fca774b9feceee54
Mathieu "ralmn" HIREL <contact.ralmn@gmail.com>
2018-01-13 17:35:02 +0100

    Add fibonnaci test

--- tests/tests_run/test_run.py
+++ tests/tests_run/test_run.py
@@ -15,24 +15,26 @@ import syntax
 
 class TestRun(unittest.TestCase):
 
-    def test_compile(self):
+
+    def test_compile_perso(self):
 
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        for dirname, dirnames, filenames in os.walk(os.path.join(dir_path, '../../tests_prog/pass')):
+        for dirname, dirnames, filenames in os.walk(os.path.join(dir_path, '../../tests_perso/pass')):
             for file in filenames:
-                if '.txt' in file:
+                if '.txt' in file or '.cmm' in file:
                     p = os.path.join(dirname, file)
                     self.runFile(p)
 
-    def test_exec(self):
+    def test_exec_perso(self):
 
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        for dirname, dirnames, filenames in os.walk(os.path.join(dir_path, '../../tests_prog/pass')):
+        for dirname, dirnames, filenames in os.walk(os.path.join(dir_path, '../../tests_perso/pass')):
 
             for file in filenames:
-                if '.txt' in file:
+                if '.txt' in file or '.cmm' in file:
                     p = os.path.join(dirname, file)
-                    pathOut = os.path.join(dirname, file.replace('.txt', '.out'))
+                    pathOut = os.path.join(dirname, file.replace('.txt', '.out').replace('.cmm', '.out'))
+
                     file = open(pathOut, 'r')
                     lines = file.readlines()
 
@@ -57,21 +59,64 @@ class TestRun(unittest.TestCase):
                     assert ''.join(lines) == ''.join(linesOut)
 
 
+    def test_compile_prof(self):
+
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        for dirname, dirnames, filenames in os.walk(os.path.join(dir_path, '../../tests_prof/pass')):
+            for file in filenames:
+                if '.txt' in file or '.cmm' in file:
+                    p = os.path.join(dirname, file)
+                    self.runFile(p, appendMain=True)
+
+    def test_exec_prof(self):
+
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        for dirname, dirnames, filenames in os.walk(os.path.join(dir_path, '../../tests_prof/pass')):
+
+            for file in filenames:
+                if '.txt' in file or '.cmm' in file:
+                    p = os.path.join(dirname, file)
+                    pathOut = os.path.join(dirname, file.replace('.txt', '.out'))
+                    file = open(pathOut, 'r')
+                    lines = file.readlines()
+
+                    process = subprocess.Popen(os.path.join(dir_path, '../../MSM/msm'),
+                                           stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+                    self.runFile(p, out=process.stdin, skip_print=False, appendMain=True)
+
+
+                    process.stdin.close()
+                    rc = process.wait()
+                    #print('rc', rc)
+
+                    assert rc == 0
+
+                    linesOut = []
+
+                    for line in process.stdout:
+                        linesOut.append(line)
+
+                    print('lines', lines, linesOut)
+                    assert ''.join(lines) == ''.join(linesOut)
+
+
     @unittest.skip("test de debug")
     def test_specifique(self):
         file = 'pass.033.txt'
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        dirname = os.path.join(dir_path, '../../tests_prog/pass')
+        dirname = os.path.join(dir_path, '../../tests_prof/pass')
         p = os.path.join(dirname, file)
         self.runFile(p, skip_print=False)
 
-    def runFile(self, path, skip_print=True, out=sys.stdout):
+    def runFile(self, path, skip_print=True, out=sys.stdout, appendMain=False):
         print('Run file %s ' % path)
 
         try:
             file = open(path, 'r')
             lines = file.readlines()
-            lines.insert(0, 'int main()')
+            if appendMain:
+                lines.insert(0, 'int main()')
             #lines.append('}')
             main.run('\n'.join(lines), skip_print=skip_print, out=out)
 
